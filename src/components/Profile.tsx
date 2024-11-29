1c38b12b7f4d2ef8175c478377f9315fecac3882
Scott McDonald <smcdo0211@gmail.com>
2024-11-29 17:50:47 -0500

    added login page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,73 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Profile.css';
+import parkingPicture from '../assets/parkingPhoto.jpg'
+import logo from '../assets/image-logo.png';
+
 
 const Profile: React.FC = () => {
   const navigate = useNavigate(); 
 
   return (
     <div className="profile-page">
-      <h1>Profile</h1>
-      <p>This is your profile page. Customize it as needed.</p>
+      <div className='stock-image'>
+        <img src={parkingPicture} alt='this is the parking lot'></img>
+      </div>
+      <div className='sign-in-container'>
+        <img src= {logo} alt='app logo'></img>
+
+      
+
+      <h1>Sign in</h1>
+
+      <div className='sign-in-form'>
+      <h3>Email</h3>
+
+<form className="search-bar">
+      <input
+        type="text"
+        placeholder="Email"
+        className="search-input"
+      />
+</form>
+<h3>Password</h3>
+  <form className="search-bar">
+   
+      <input
+        type="text"
+        placeholder="Password"
+        className="search-input"
+      />
+      </form>
+
+    
+
+   
+
+
+
+
+     
       <button 
         className="return-button" 
         onClick={() => navigate('/')}
       >
-        Return to Main Page
+        Log in
       </button>
+      <div className='login-sso'>
+      <hr />
+      <h3>Log in with Others</h3>
+      <hr />
+     </div>
+
+     <button className='sso-button'>Log in with SSO</button>
+
+
+
+    </div>
+    </div>
+
+   
     </div>
   );
 };
